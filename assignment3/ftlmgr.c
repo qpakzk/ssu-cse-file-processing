9024597dd9b9e00b62a37e8f0115fa1a19d5b54f
Sangwon Hong <qpakzk@gmail.com>
2018-04-17 10:25:29 +0900

    Delete comments and add exception
    Signed-off-by: Sangwon Hong <qpakzk@gmail.com>

--- assignment3/ftlmgr.c
+++ assignment3/ftlmgr.c
@@ -15,7 +15,6 @@ FILE *devicefp;
 int backup_blk = BLOCKS_PER_DEVICE - 1;
 bool pbn_flag[BLOCKS_PER_DEVICE];
 
-/****************  prototypes ****************/
 void ftl_open();
 void ftl_write(int lsn, char *sectorbuf);
 void ftl_read(int lsn, char *sectorbuf);
@@ -28,25 +27,19 @@ int dd_write(int ppn, char *pagebuf);
 int dd_erase(int pbn);
 
 bool is_full();
+int copy_block(int dest_pbn, int src_pbn);
 int is_same_lsn(int lsn);
-//
-// flash memory를 처음 사용할 때 필요한 초기화 작업, 예를 들면 address mapping table에 대한
-// 초기화 등의 작업을 수행한다
-//
+
 void ftl_open()
 {
 	int i;
 
-	// initialize the address mapping table
 	for(i = 0; i < DATABLKS_PER_DEVICE; i++)
 	{
 		blkmaptbl.entry[i].pbn = -1;
 		blkmaptbl.entry[i].first_free_page_offset = 0;
 	}
 
-	//
-	// 추가적으로 필요한 작업이 있으면 수행할 것
-	//
 	memset(pbn_flag, false, sizeof(pbn_flag));
 	return;
 }
@@ -116,13 +109,23 @@ int is_same_lsn(int lsn) {
 
 void ftl_write(int lsn, char *sectorbuf)
 {
-	int lbn = lsn / SECTORS_PER_BLOCK;
+	int lbn;
 	int i;
 	int ppn;
 
-	char *pagebuf = (char *)malloc(PAGE_SIZE);
-	SpareData *sdata = (SpareData *)malloc(SPARE_SIZE);
+	char *pagebuf;
+	SpareData *sdata;
+	
+	if(lsn < 0 || lsn >= DATABLKS_PER_DEVICE * PAGES_PER_BLOCK) {
+		fprintf(stderr, "Logical Sector Number %d is out of range.\n", lsn);
+		exit(1);
+	}
 
+	lbn = lsn / SECTORS_PER_BLOCK; 
+
+	pagebuf = (char *)malloc(PAGE_SIZE);
+	sdata = (SpareData *)malloc(SPARE_SIZE);
+		
 	if(blkmaptbl.entry[lbn].pbn == -1) {
 		for(i = 0; i < BLOCKS_PER_DEVICE; i++) {
 			if(!pbn_flag[i]) {
@@ -232,7 +235,7 @@ void ftl_write(int lsn, char *sectorbuf)
 		}
 	}
 
-#ifdef PRINT_FOR_DEBUG				// 필요 시 현재의 block mapping table을 출력해 볼 수 있음
+#ifdef PRINT_FOR_DEBUG
 	print_blkmaptbl();
 
 	for(i = 0; i < BLOCKS_PER_DEVICE; i++) 
@@ -246,14 +249,23 @@ void ftl_write(int lsn, char *sectorbuf)
 
 void ftl_read(int lsn, char *sectorbuf)
 {
-	int lbn = lsn / PAGES_PER_BLOCK;	
-	int pbn = blkmaptbl.entry[lbn].pbn;
-	int first_ppn = pbn * PAGES_PER_BLOCK;
+	int lbn;	
+	int pbn;
+	int first_ppn;
 	char pagebuf[PAGE_SIZE];
 	SpareData sdata[SPARE_SIZE];
 	int ppn;
 	int i;
 	
+	if(lsn < 0 || lsn >= DATABLKS_PER_DEVICE * PAGES_PER_BLOCK) {
+		fprintf(stderr, "Logical Sector Number %d is out of range.\n", lsn);
+		exit(1);
+	}
+
+	lbn = lsn / PAGES_PER_BLOCK;	
+	pbn = blkmaptbl.entry[lbn].pbn;
+	first_ppn = pbn * PAGES_PER_BLOCK;
+
 	for(i = 0; i < PAGES_PER_BLOCK; i++) {
 		ppn = first_ppn + i;
 		dd_read(ppn, pagebuf);
@@ -265,17 +277,14 @@ void ftl_read(int lsn, char *sectorbuf)
 	}
 
 	fprintf(stderr, "There's no data mapping with logical sector number %d\n", lsn);
-#ifdef PRINT_FOR_DEBUG				// 필요 시 현재의 block mapping table을 출력해 볼 수 있음
-	//print_blkmaptbl();
+#ifdef PRINT_FOR_DEBUG
+	print_blkmaptbl();
 	print_block(1);
 #endif
 
 	return;
 }
 
-//
-// initialize flash memory where each byte are set to 'OxFF'
-// 
 void initialize_flash_memory()
 {
 	char *blockbuf;
