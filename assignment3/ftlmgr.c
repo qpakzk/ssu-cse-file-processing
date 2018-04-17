3c1f3bc57a6e9857144555fcc5c9101d16ab7760
Sangwon Hong <qpakzk@gmail.com>
2018-04-17 09:56:18 +0900

    Complete to implement FTL functions
    Signed-off-by: Sangwon Hong <qpakzk@gmail.com>

--- assignment3/ftlmgr.c
+++ assignment3/ftlmgr.c
@@ -1,4 +1,4 @@
-#define PRINT_FOR_DEBUG
+//#define PRINT_FOR_DEBUG
 
 #include <stdio.h>
 #include <string.h>
@@ -15,11 +15,6 @@ FILE *devicefp;
 int backup_blk = BLOCKS_PER_DEVICE - 1;
 bool pbn_flag[BLOCKS_PER_DEVICE];
 
-typedef struct {
-	char sector_area[SECTOR_SIZE * SECTORS_PER_PAGE];
-	SpareData spare_area;
-} PageData;
-
 /****************  prototypes ****************/
 void ftl_open();
 void ftl_write(int lsn, char *sectorbuf);
@@ -28,8 +23,12 @@ void initialize_flash_memory();
 void print_block(int pbn);
 void print_blkmaptbl();
 
-int dd_write(int, char *);
-int dd_read(int, char *);
+int dd_read(int ppn, char *pagebuf);
+int dd_write(int ppn, char *pagebuf);
+int dd_erase(int pbn);
+
+bool is_full();
+int is_same_lsn(int lsn);
 //
 // flash memory를 처음 사용할 때 필요한 초기화 작업, 예를 들면 address mapping table에 대한
 // 초기화 등의 작업을 수행한다
@@ -52,92 +51,77 @@ void ftl_open()
 	return;
 }
 
-/*
 bool is_full() {
 	int i;
 	for(i = 0; i < BLOCKS_PER_DEVICE; i++) {
 		if(i == backup_blk)
 			continue;
 		
-		if(!pbn_table[i].flag)
+		if(!pbn_flag[i])
 			return false;
 	}
-
 	return true;
 }
 
-bool have_invalids() {
-	int i;
-	for(i = 0; i < BLOCKS_PER_DEVICE; i++) {
-		if(i == backup_blk)
-			continue;
+int copy_block(int dest_pbn, int src_pbn) {
+	char *pagebuf = (char *)malloc(PAGE_SIZE);
+	SpareData *sdata = (SpareData *)malloc(SPARE_SIZE);
 
-		if(pbn_flag[i].num_of_invalids) 
-			return true;
-	}
+	int i, j;
+	int dest_first_ppn =  PAGES_PER_BLOCK * dest_pbn;
+	int src_first_ppn =  PAGES_PER_BLOCK * src_pbn;
 	
-	return false;
-}
-
-int select_block() {
-	int pbn = -1;
-	int i;
-
-	for(i = 0; i < BLOCKS_PER_DEVICE; i++) {
-		if(i == backup_blk)
-			continue;
-
-		if(pbn_table[i].num_of_invalids > 0 && pbn < pbn_table[i].num_of_invalids)
-			pbn = i;
-	}
-
-	return pbn;
-}
-
-int cal_ppn(int pbn) {
-	int ppn;
-	int i;
+	j = 0;
+	for(i = 0; i < SECTORS_PER_BLOCK; i++) {
+		memset(pagebuf, 0, PAGE_SIZE);
+		dd_read(src_first_ppn + i, pagebuf);
 		
-}
-void copy_valid_pages(int pbn) {
-	PageData page;
-	int i;
-
-	for(i = 0; i < PAGES_PER_BLOCK; i++) {
-		dd_read(pbn * BLOCK_SIZE + i, (char *)&page);
-		if(page.spare_data.invalid == -1) {
-			dd_write(backup_blk * BLOCK_SIZE + 
+		memset(sdata, 0, SPARE_SIZE);
+		memcpy(sdata, pagebuf+SECTOR_SIZE, SPARE_SIZE);
+		if(sdata->invalid == -1) {
+			dd_write(dest_first_ppn + j, pagebuf);
+			j++;
 		}
-	SpareData spare_data;
 	}
+
+	free(pagebuf);
+	free(sdata);
+	return j;
 }
 
-void erase_block() {
+int is_same_lsn(int lsn) {
+	int i;
+	int lbn = lsn / SECTORS_PER_BLOCK;
+	int pbn = blkmaptbl.entry[lbn].pbn;
+	int first_ppn = pbn * PAGES_PER_BLOCK;
+	int first_free_page_offset = blkmaptbl.entry[lbn].first_free_page_offset;
 
+	char *pagebuf = (char *)malloc(PAGE_SIZE);
+	SpareData *sdata = (SpareData *)malloc(SPARE_SIZE);
+
+	for(i = first_ppn; i < first_ppn + first_free_page_offset; i++) {
+		dd_read(i, pagebuf);
+
+		memcpy(sdata, pagebuf+SECTOR_SIZE, SPARE_SIZE);
+		if(sdata->invalid == -1) {
+			if(lsn == sdata->lsn)
+				return i;
+		}
+	}	
+
+	free(pagebuf);
+	free(sdata);
+	return -1;		
 }
-*/
+
 void ftl_write(int lsn, char *sectorbuf)
 {
-	int lbn = lsn / BLOCK_SIZE;
-	int i, j;
+	int lbn = lsn / SECTORS_PER_BLOCK;
+	int i;
 	int ppn;
-	bool has_same_lsn;
-	PageData pagebuf;
-/*
-	if(is_full()) {
-		if(!have_invalids()) {
-			fprintf(stderr, "Full of valid data\n");
-			exit(1);
-		}
-		
-		pbn = select_block();
-		copy_valid_pages(pbn);
-		erase_block();
-	}
-	else */
 
-	memset((char *)&pagebuf, 0, sizeof(PageData));
-	has_same_lsn = false;
+	char *pagebuf = (char *)malloc(PAGE_SIZE);
+	SpareData *sdata = (SpareData *)malloc(SPARE_SIZE);
 
 	if(blkmaptbl.entry[lbn].pbn == -1) {
 		for(i = 0; i < BLOCKS_PER_DEVICE; i++) {
@@ -147,72 +131,140 @@ void ftl_write(int lsn, char *sectorbuf)
 
 				pbn_flag[i] = true;
 				blkmaptbl.entry[lbn].pbn = i;
-				blkmaptbl.entry[lbn].first_free_page_offset++;
 
-				strncpy(pagebuf.sector_area, sectorbuf, SECTOR_SIZE * SECTORS_PER_PAGE);
-				pagebuf.spare_area.lsn = lsn;
-				pagebuf.spare_area.invalid = -1;	
-				
-				ppn = (blkmaptbl.entry[lbn].first_free_page_offset - 1) + BLOCK_SIZE * blkmaptbl.entry[lbn].pbn;
-				dd_write(ppn, (char *)&pagebuf);
+				memset(pagebuf, 0xFF, PAGE_SIZE);
+				memcpy(pagebuf, sectorbuf, SECTOR_SIZE);
+
+				memset((char *)sdata, 0xFF, SPARE_SIZE);
+				sdata->lsn = lsn;
+				sdata->invalid = -1;
+				memcpy(pagebuf+SECTOR_SIZE, sdata, SPARE_SIZE);
+
+				ppn = blkmaptbl.entry[lbn].first_free_page_offset + PAGES_PER_BLOCK * blkmaptbl.entry[lbn].pbn;
+				blkmaptbl.entry[lbn].first_free_page_offset++;
+				dd_write(ppn, pagebuf);
 				break;
 			}
 		}
 	}
 	else if(blkmaptbl.entry[lbn].first_free_page_offset < PAGES_PER_BLOCK) {
-		for(i = 0; i < blkmaptbl.entry[lbn].first_free_page_offset; i++) {
-			ppn = i + BLOCK_SIZE * blkmaptbl.entry[lbn].pbn;
-			dd_read(ppn, (char *)&pagebuf);
-			if(pagebuf.spare_area.invalid == -1) {
-				pagebuf.spare_area.invalid = 1;
-				break;
-			}
+		if((ppn = is_same_lsn(lsn)) != -1) {
+			dd_read(ppn, pagebuf);	
+			memcpy(sdata, pagebuf+SECTOR_SIZE, SPARE_SIZE);
+			sdata->invalid = 1;
+			memcpy(pagebuf+SECTOR_SIZE, sdata, SPARE_SIZE);
+			dd_write(ppn, pagebuf);
+		}			
 
-			
-		}
-		blkmaptbl.entry[lbn].first_free_page_offset++;
-		
-		strncpy(pagebuf.sector_area, sectorbuf, SECTOR_SIZE * SECTORS_PER_PAGE);
-		pagebuf.spare_area.lsn = lsn;
-		pagebuf.spare_area.invalid = -1;	
-			
-		memset((char *)&pagebuf, 0, sizeof(PageData));
-		ppn = (blkmaptbl.entry[lbn].first_free_page_offset - 1) + BLOCK_SIZE * blkmaptbl.entry[lbn].pbn;
-		dd_write(ppn, (char *)&pagebuf);
-	}
-	/*
-	else {
-		for(i = 0; i < DATABLKS_PER_DEVICE; i++) {
-			if() {
-				
-			}
-		}
-	}
-		ppn = blkmaptbl.entry[lbn].first_free_page_offset + lbn * BLOCK_SIZE;
-		blkmaptbl.entry[lbn].pbn = lbn;
-		blkmaptbl.entry[lbn].first_free_page_offset += 1;
+		memset(pagebuf, 0xFF, PAGE_SIZE);
+		memcpy(pagebuf, sectorbuf, SECTOR_SIZE);
+
+		memset((char *)sdata, 0xFF, SPARE_SIZE);
+		sdata->lsn = lsn;
+		sdata->invalid = -1;
+		memcpy(pagebuf+SECTOR_SIZE, sdata, SPARE_SIZE);
 	
-	else {
-		for(i = 0; i < BLOCKS_PER_DEVICE; i++) {
-			if(blkmaptbl.entry[i].first_free_page_offest < PAGES_PER_BLOCK) {
-				ppn = blkmaptbl.entry[i].first_free_page_offset + lbn * BLOCK_SIZE;
-				blkmaptbl.entry[i].pbn = i;
-				blkmaptbl.entry[i].first_free_page_offset += 1;
-				break;
+		ppn = blkmaptbl.entry[lbn].first_free_page_offset + PAGES_PER_BLOCK * blkmaptbl.entry[lbn].pbn;
+		blkmaptbl.entry[lbn].first_free_page_offset++;
+		dd_write(ppn, pagebuf);
+	}
+	else if(blkmaptbl.entry[lbn].first_free_page_offset >= PAGES_PER_BLOCK) {
+		if((ppn = is_same_lsn(lsn)) != -1) {
+			dd_read(ppn, pagebuf);		
+			memcpy(sdata, pagebuf+SECTOR_SIZE, SPARE_SIZE);
+			sdata->invalid = 1;
+			memcpy(pagebuf+SECTOR_SIZE, sdata, SPARE_SIZE);
+			dd_write(ppn, pagebuf);
+		}			
+
+		if(is_full()) {
+			blkmaptbl.entry[lbn].first_free_page_offset = copy_block(backup_blk, blkmaptbl.entry[lbn].pbn);
+			pbn_flag[backup_blk] = true;
+			pbn_flag[blkmaptbl.entry[lbn].pbn] = false;
+
+			dd_erase(blkmaptbl.entry[lbn].pbn);
+
+			blkmaptbl.entry[lbn].pbn = backup_blk;
+			backup_blk = blkmaptbl.entry[lbn].pbn;	
+
+			memset(pagebuf, 0xFF, PAGE_SIZE);
+			memcpy(pagebuf, sectorbuf, SECTOR_SIZE);
+
+			memset((char *)sdata, 0xFF, SPARE_SIZE);
+			sdata->lsn = lsn;
+			sdata->invalid = -1;
+
+			memcpy(pagebuf+SECTOR_SIZE, sdata, SPARE_SIZE);
+			ppn = blkmaptbl.entry[lbn].first_free_page_offset + PAGES_PER_BLOCK * blkmaptbl.entry[lbn].pbn;
+			blkmaptbl.entry[lbn].first_free_page_offset++;
+			dd_write(ppn, pagebuf);
+		}
+		else {
+			for(i = 0; i < BLOCKS_PER_DEVICE; i++) {
+				if(!pbn_flag[i]) {
+					if(i == backup_blk)
+						continue;
+
+					blkmaptbl.entry[lbn].first_free_page_offset = copy_block(i, blkmaptbl.entry[lbn].pbn);
+
+					pbn_flag[i] = true;
+					pbn_flag[blkmaptbl.entry[lbn].pbn] = false;
+
+					dd_erase(blkmaptbl.entry[lbn].pbn);
+					
+					blkmaptbl.entry[lbn].pbn = i;
+
+					memset(pagebuf, 0xFF, PAGE_SIZE);
+					memcpy(pagebuf, sectorbuf, SECTOR_SIZE);
+
+					memset((char *)sdata, 0xFF, SPARE_SIZE);
+					sdata->lsn = lsn;
+					sdata->invalid = -1;
+					
+					memcpy(pagebuf+SECTOR_SIZE, sdata, SPARE_SIZE);
+					ppn = blkmaptbl.entry[lbn].first_free_page_offset + PAGES_PER_BLOCK * blkmaptbl.entry[lbn].pbn;
+					blkmaptbl.entry[lbn].first_free_page_offset++;
+					dd_write(ppn, pagebuf);
+					
+					break;
+				}
 			}
 		}
-	}	
-	*/
+	}
+
 #ifdef PRINT_FOR_DEBUG				// 필요 시 현재의 block mapping table을 출력해 볼 수 있음
 	print_blkmaptbl();
+
+	for(i = 0; i < BLOCKS_PER_DEVICE; i++) 
+		print_block(i);
 #endif
+	free(pagebuf);
+	free(sdata);
 
 	return;
 }
 
 void ftl_read(int lsn, char *sectorbuf)
 {
+	int lbn = lsn / PAGES_PER_BLOCK;	
+	int pbn = blkmaptbl.entry[lbn].pbn;
+	int first_ppn = pbn * PAGES_PER_BLOCK;
+	char pagebuf[PAGE_SIZE];
+	SpareData sdata[SPARE_SIZE];
+	int ppn;
+	int i;
+	
+	for(i = 0; i < PAGES_PER_BLOCK; i++) {
+		ppn = first_ppn + i;
+		dd_read(ppn, pagebuf);
+		memcpy(sdata, pagebuf+SECTOR_SIZE, SPARE_SIZE);
+		if(sdata->invalid == -1 && sdata->lsn == lsn) {
+			memcpy(sectorbuf, pagebuf, SECTOR_SIZE);	
+			return;
+		}
+	}
 
+	fprintf(stderr, "There's no data mapping with logical sector number %d\n", lsn);
 #ifdef PRINT_FOR_DEBUG				// 필요 시 현재의 block mapping table을 출력해 볼 수 있음
 	//print_blkmaptbl();
 	print_block(1);
