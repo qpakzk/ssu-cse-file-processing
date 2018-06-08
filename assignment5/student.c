d23b9775e715fcdd2adde7f807d6157052e38dce
Sangwon Hong <qpakzk@gmail.com>
2018-06-08 11:45:27 +0900

    Implement kwaymerge, but don't work well
    Signed-off-by: Sangwon Hong <qpakzk@gmail.com>

--- assignment5/student.c
+++ assignment5/student.c
@@ -4,7 +4,20 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdbool.h>
 #include "student.h"
+
+typedef struct _RUN {
+	FILE *runfp;
+	//for run
+	int chunkid;
+	int num_of_records_in_run;
+	bool end_run;
+	//for inputbuf
+	int index;
+	int num_of_records_in_buf;
+	bool end_chunk;
+} RUN;
 
 void readHeader(FILE *fp, char *headerbuf);
 void makeRuns(FILE *inputfp, char *inputbuf);
@@ -12,6 +25,10 @@ void parse_inputbuf(char *inputbuf, STUDENT *s, int n);
 void merge_inputbuf(char *inputbuf, STUDENT *s, int n);
 int compare_keyval(const void *st1, const void *st2);
 void internalsort(char *inputbuf, int n);
+int find_keyval(char *inputbuf, int index);
+void initialize_output(FILE *outputfp);
+void write_to_output(FILE *outputfp, char *inputbuf);
+void initialize_run(RUN *run_file);
 void kwaymerge(FILE *outputfp, char *inputbuf, char *outputbuf);
 void pack(char *recordbuf, const STUDENT *s);
 void unpack(const char *recordbuf, STUDENT *s);
@@ -21,7 +38,8 @@ void removeAllRuns();
 void print_inputbuf(char *inputbuf);
 void print_records(STUDENT *s, int n);
 
-int run_count = 0;
+int run_count;
+int chunk_size;
 
 int main(int argc, char *argv[]) {
 	FILE *inputfp, *outputfp;
@@ -59,16 +77,16 @@ int main(int argc, char *argv[]) {
 		}
 
 		fclose(inputfp);
-/*
-		if((outputfp = fopen(new_record_file, "w")) == NULL) {
+
+		if((outputfp = fopen(new_record_file, "r+")) == NULL) {
 			fprintf(stderr, "open error for %s\n", new_record_file);
 			exit(1);
 		}
 		
 		kwaymerge(outputfp, inputbuf, outputbuf);
-		fclose(outputfp);*/
+		fclose(outputfp);
 	}
-	removeAllRuns();
+	//removeAllRuns();
 	return 1;
 }
 
@@ -169,8 +187,169 @@ void internalsort(char *inputbuf, int n) {
 #endif
 }
 
-void kwaymerge(FILE *outputfp, char *inputbuf, char *outputbuf) {
+int find_keyval(char *inputbuf, int index) {
+	char recordbuf[RECORD_SIZE];
+	char *token;
+	int keyval;
 
+	memcpy(recordbuf, inputbuf + index * RECORD_SIZE, RECORD_SIZE);
+	token = strtok(recordbuf, "#");
+	if(!token)
+		return -1;
+	keyval = atoi(token);
+	printf("keyval = %d\n", keyval);
+	return keyval;
+}
+
+void initialize_output(FILE *outputfp) {
+	char headerbuf[HEADER_SIZE];
+	short num_of_records = 0;
+	short del_rrn = -1;
+
+	memset(headerbuf, 0x00, HEADER_SIZE);
+	headerbuf[0] = (char)((num_of_records & 0xFF00) >> 8);
+	headerbuf[1] = (char)(num_of_records & 0xFF);
+	headerbuf[2] = (char)((del_rrn & 0xFF00) >> 8);
+	headerbuf[3] = (char)(del_rrn & 0xFF);
+
+	fseek(outputfp, 0, SEEK_SET);
+	fwrite(headerbuf, sizeof(char), HEADER_SIZE, outputfp);
+}
+
+void write_to_output(FILE *outputfp, char *inputbuf) {
+	char recordbuf[RECORD_SIZE];
+	char headerbuf[HEADER_SIZE];
+	short num_of_records;
+	short del_rrn;
+	int i;
+
+	memset(headerbuf, 0x00, HEADER_SIZE);
+	fseek(outputfp, 0, SEEK_SET);
+	fread(headerbuf, sizeof(char), HEADER_SIZE, outputfp);
+
+	num_of_records = ((headerbuf[0] << 8) & 0xFF00) + (headerbuf[1] & 0xFF);
+	del_rrn = ((headerbuf[2] << 8) & 0xFF00) + (headerbuf[3] & 0xFF);
+
+	memset(recordbuf, 0x00, RECORD_SIZE);
+	memcpy(recordbuf, inputbuf, RECORD_SIZE);
+
+	fseek(outputfp, HEADER_SIZE + num_of_records * RECORD_SIZE, SEEK_SET);
+	fwrite(recordbuf, sizeof(char), RECORD_SIZE, outputfp);
+
+	num_of_records++;
+
+	headerbuf[0] = (char)((num_of_records & 0xFF00) >> 8);
+	headerbuf[1] = (char)(num_of_records & 0xFF);
+	headerbuf[2] = (char)((del_rrn & 0xFF00) >> 8);
+	headerbuf[3] = (char)(del_rrn & 0xFF);
+
+	fseek(outputfp, 0, SEEK_SET);
+	fwrite(headerbuf, sizeof(char), HEADER_SIZE, outputfp);
+}
+void initialize_run(RUN *run_file) {
+	int i;
+	char run_name[RUN_NAME_SIZE];
+	int file_size;
+	int chunk_size;
+	memset(run_file, 0x00, sizeof(RUN) * run_count);
+
+	for (i = 0; i < run_count; i++) {
+		memset(run_name, 0x00, RUN_NAME_SIZE);
+		snprintf(run_name, RUN_NAME_SIZE, "run%d.dat", i);
+		if ((run_file[i].runfp = fopen(run_name, "r")) == NULL) {
+			fprintf(stderr, "open error for %s\n", run_name);
+			exit(1);
+		}
+	}
+
+	for(i = 0; i < run_count; i++) {
+		fseek(run_file[i].runfp, 0, SEEK_END);
+		file_size = ftell(run_file[i].runfp);
+		run_file[i].num_of_records_in_run = file_size / RECORD_SIZE;
+	}
+
+	chunk_size = INPUT_BUF_SIZE / run_count;
+	for(i = 0; i < run_count; i++)
+		run_file[i].num_of_records_in_buf = chunk_size / RECORD_SIZE;
+}
+
+void kwaymerge(FILE *outputfp, char *inputbuf, char *outputbuf) {
+	int i;
+	int run_num;
+	RUN run_file[run_count];
+	int lowest_run;
+	int lowest_key, candidate_key;
+	bool is_sorted;
+
+	chunk_size = INPUT_BUF_SIZE / run_count;
+	if (chunk_size < RECORD_SIZE) {
+		fprintf(stderr, "Chunk size s smaller than record size.\n");
+		exit(1);
+	}
+	initialize_run(run_file);
+
+	memset(inputbuf, 0x00, INPUT_BUF_SIZE);
+	memset(outputbuf, 0x00, OUTPUT_BUF_SIZE);
+	initialize_output(outputfp);
+
+	for (i = 0; i < run_count; i++)
+		readChunk(run_file[i].runfp, &inputbuf[i * chunk_size], run_file[i].chunkid++);
+
+	run_num = 0;
+	while(1) {
+		while(run_file[run_num].end_run)
+			run_num++;
+
+		lowest_run = run_num;
+		lowest_key = find_keyval(inputbuf, run_file[run_num].index);
+		printf("lowest_run = %d, lowest_key = %d\n", lowest_run, lowest_key);
+		for (i = run_num + 1; i < run_count; i++) {
+			if(run_file[i].end_run)
+				continue;
+			candidate_key = find_keyval(&inputbuf[i * chunk_size], run_file[i].index);
+			if(candidate_key == -1)
+				break;
+			if(lowest_key > candidate_key) {
+				lowest_key = candidate_key;
+				lowest_run = i;
+			}
+		}
+		printf("lowest_run = %d, lowest_key = %d, candidate_key = %d\n", lowest_run, lowest_key, lowest_key);
+		printf("----------------------------------------------------\n\n");
+		//write_to_output(outputfp, &inputbuf[lowest_run * chunk_size]);
+		
+		run_file[lowest_run].index++;
+		if(run_file[lowest_run].index >= run_file[lowest_run].num_of_records_in_buf)
+			run_file[lowest_run].end_chunk = true;
+
+		if (run_file[lowest_run].end_chunk) {
+			run_file[lowest_run].index = 0;
+			run_file[lowest_run].end_chunk = false;
+			if (run_file[lowest_run].chunkid * run_file[lowest_run].num_of_records_in_buf >= run_file[lowest_run].num_of_records_in_run) {
+				run_file[lowest_run].end_run = true;
+				for (i = lowest_run * chunk_size; i < chunk_size; i++)
+					inputbuf[i] = 0;
+				
+				is_sorted = true;
+				for (i = 0; i < run_count; i++) {
+					if (run_file[i].end_run == false) {
+						is_sorted = false;
+						break;
+					}
+				}
+				
+				if (is_sorted) {
+					memset(inputbuf, 0x00, INPUT_BUF_SIZE);
+					break;
+				}
+			}
+			else
+				readChunk(run_file[lowest_run].runfp, &inputbuf[lowest_run * chunk_size], run_file[lowest_run].chunkid++);
+		}
+	}
+
+	for (i = 0; i < run_count; i++)
+		fclose(run_file[i].runfp);
 }
 
 void pack(char *recordbuf, const STUDENT *s) {
@@ -222,10 +401,15 @@ void unpack(const char *recordbuf, STUDENT *s) {
 }
 
 void readChunk(FILE *runfp, char *inputbuf, int chunkid) {
-	int records_per_chunk = RECORDS_NUM_INPUTBUF / run_count;
-	int input_index = chunkid * records_per_chunk * RECORD_SIZE;
-	int chunk_size = records_per_chunk * RECORD_SIZE;
-	fread(&inputbuf[input_index], sizeof(char), chunk_size, runfp);
+	int num_of_records;
+
+	num_of_records = chunk_size / RECORD_SIZE;
+	fseek(runfp, chunkid * num_of_records * RECORD_SIZE, SEEK_SET);
+	fread(inputbuf, sizeof(char), num_of_records * RECORD_SIZE, runfp);
+	printf("INNER : ");
+	for (int i = 0; i < num_of_records * RECORD_SIZE; i++)
+		printf("%c", inputbuf[i]);
+	printf("\n");
 }
 
 void writeOutputbuf(FILE *outputfp, const char *outputbuf, int n) {
@@ -244,6 +428,7 @@ void removeAllRuns() {
 		}
 	}
 }
+
 void print_inputbuf(char *inputbuf) {
 	int i;
 	for(i = 0; i < INPUT_BUF_SIZE; i++)
